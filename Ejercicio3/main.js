90fa528a6a6d39394630009186065b8563df79e5
Miguel Angel Castro Escamilla <ma98030965140@gmail.com>
2021-02-03 20:02:38 -0500

    Soluciones

---
+++ Ejercicio3/main.js
@@ -0,0 +1,6 @@
+
+// 3.Realizar un programa que pueda sumar dos números leídos por teclado y escribir el resultado.
+
+let num1 = parseInt(prompt("Ingrese el numero 1"));
+let num2 = parseInt(prompt("Ingrese el numero 2"));
+console.log(`${num1} + ${num2} = ${num1+num2}`);
